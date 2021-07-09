01aac94d1b063e03d1a61b85c914d379f51430ab
Max Nowack <max.nowack@gmail.com>
2021-07-09 12:25:41 +0200

    fix parameter name

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,7 +1,7 @@
 import core from '@actions/core'
 import github from '@actions/github'
 
-const token = core.getInput('repo_token', { required: true })
+const token = core.getInput('token', { required: true })
 const octokit = github.getOctokit(token).rest
 
 export default octokit
